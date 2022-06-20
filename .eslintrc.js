1e7218b24133a18ce4b2ca2d21b0f56ec645c4c7
my <email@email.ru>
2022-06-20 17:47:01 +0300

    3-cols maket with developer environment

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     sourceType: "module",
   },
   rules: {
-    indent: ["error", "tab"],
+    indent: ["error", 2],
     "linebreak-style": ["error", "unix"],
     quotes: ["error", "double"],
     semi: ["error", "always"],
