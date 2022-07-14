ac55e6cf4eaad8f7684a26e1d457c07e1283556b
my <email@email.ru>
2022-07-14 15:39:31 +0300

    functional programmind with parcel bundler for codesandbox

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -7,43 +7,43 @@ describe("sum function testing", () => {
   });
 
   it("tests that sum function get params and do it all right", () => {
-    jest.spyOn(window, "alert").mockImplementation(() => {});
+    jest.spyOn(console, "log").mockImplementation(() => {});
 
-    alert(sum().toString()); // "0";
-    expect(window.alert).toHaveBeenCalledWith("0");
-    alert(sum().valueOf()); // 0;
-    expect(window.alert).toHaveBeenCalledWith(0);
+    console.log(sum().toString()); // "0";
+    expect(console.log).toHaveBeenCalledWith("0");
+    console.log(sum().valueOf()); // 0;
+    expect(console.log).toHaveBeenCalledWith(0);
     const s = sum();
-    alert(s(1).toString()); // 1
-    expect(window.alert).toHaveBeenCalledWith("1");
-    alert(s(1)(2).toString()); //3
-    expect(window.alert).toHaveBeenCalledWith("3");
-    alert(s(3)(4)(5).toString()); // 12
-    expect(window.alert).toHaveBeenCalledWith("12");
+    console.log(s(1).toString()); // 1
+    expect(console.log).toHaveBeenCalledWith("1");
+    console.log(s(1)(2).toString()); //3
+    expect(console.log).toHaveBeenCalledWith("3");
+    console.log(s(3)(4)(5).toString()); // 12
+    expect(console.log).toHaveBeenCalledWith("12");
     const s3 = sum(3);
-    alert(s3(5).toString()); // 8
-    expect(window.alert).toHaveBeenCalledWith("8");
-    alert(s3(6).toString()); // 9
-    expect(window.alert).toHaveBeenCalledWith("9");
+    console.log(s3(5).toString()); // 8
+    expect(console.log).toHaveBeenCalledWith("8");
+    console.log(s3(6).toString()); // 9
+    expect(console.log).toHaveBeenCalledWith("9");
   });
 
   it("tests that sum function get another params and do it all right", () => {
-    jest.spyOn(window, "alert").mockImplementation(() => {});
+    jest.spyOn(console, "log").mockImplementation(() => {});
 
     const s9 = sum(9);
-    alert(s9().toString()); // "9"
-    expect(alert).toHaveBeenCalledWith("9");
-    alert(s9().valueOf()); // 9
-    expect(alert).toHaveBeenCalledWith(9);
-    alert(s9(-1).toString()); //"8"
-    expect(alert).toHaveBeenCalledWith("8");
-    alert(s9(-1).valueOf()); //8
-    expect(alert).toHaveBeenCalledWith(8);
-    alert(s9(-1)().toString()); //"8"
-    expect(alert).toHaveBeenCalledWith("8");
-    alert(s9("-1")().toString()); //"8"
-    expect(alert).toHaveBeenCalledWith("8");
-    alert(s9("-1").valueOf()); //8
-    expect(alert).toHaveBeenCalledWith(8);
+    console.log(s9().toString()); // "9"
+    expect(console.log).toHaveBeenCalledWith("9");
+    console.log(s9().valueOf()); // 9
+    expect(console.log).toHaveBeenCalledWith(9);
+    console.log(s9(-1).toString()); //"8"
+    expect(console.log).toHaveBeenCalledWith("8");
+    console.log(s9(-1).valueOf()); //8
+    expect(console.log).toHaveBeenCalledWith(8);
+    console.log(s9(-1)().toString()); //"8"
+    expect(console.log).toHaveBeenCalledWith("8");
+    console.log(s9("-1")().toString()); //"8"
+    expect(console.log).toHaveBeenCalledWith("8");
+    console.log(s9("-1").valueOf()); //8
+    expect(console.log).toHaveBeenCalledWith(8);
   });
 });
